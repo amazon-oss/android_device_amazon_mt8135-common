bfc1821bab0896cd3e09c94912773985ad42f07f
R0rt1z2 <me@r0rt1z2.com>
2024-05-20 15:11:03 +0200

    mt8135-common: Set Architecture flags

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -6,5 +6,13 @@
 
 COMMON_PATH := device/amazon/mt8135-common
 
+# Architecture
+TARGET_ARCH := arm
+TARGET_ARCH_VARIANT := armv7-a-neon
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
+TARGET_CPU_VARIANT := cortex-a7
+TARGET_ARCH_VARIANT_CPU := cortex-a15
+
 # Inherit the proprietary files
 include vendor/amazon/mt8135-common/BoardConfigVendor.mk
